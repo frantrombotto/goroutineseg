bd23095f5c093928c6eb42722eb947306fadab7c
ftrombotto <franco.trombotto@mercadolibre.com>
2019-10-13 00:51:11 -0300

    Add method that reads user ids from csv file and outputs ML users nicknames

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -27,8 +28,42 @@ type ApiResp struct {
 }
 
 func main() {
+	PrintUserNicknamesFromCsv()
+}
+
+func createNewFile(newFileName string) {
+	newFile, err := os.Create(newFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	newFile.Close()
+}
+
+func openFile(inputFileName string) *os.File {
+	file, err := os.Open(inputFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
+}
+
+func writeLine(inputFileName, line string) {
+	newFile, err := os.OpenFile(inputFileName, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer newFile.Close()
+	newFile.WriteString(line+"\n")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Prints data from different cryptocoins to a nue file
+// Uses goroutines to make API calls concurrently
+func GetCryptoData() {
 	fileName := "newfile.csv"
-	CreateNewFile(fileName)
+	createNewFile(fileName)
 
 	client := &http.Client{}
 	client.Timeout = time.Second * 15
@@ -60,35 +95,58 @@ func main() {
 
 	for i := 0; i < len(coins); i++ {
 		response := <-respChan
-		WriteLine(fileName, fmt.Sprintf("%v", response))
+		writeLine(fileName, fmt.Sprintf("%v", response))
 	}
-
 }
 
-func CreateNewFile(newFileName string) {
-	newFile, err := os.Create(newFileName)
+
+// Reads csv input file with userids
+// and returns it's nicknames.
+// Uses goroutines and limits workers
+func PrintUserNicknamesFromCsv()  {
+	type UserAPIResp struct {
+		Nickname   string `json:"nickname"`
+	}
+
+	fileName := "output.csv"
+	createNewFile(fileName)
+
+	inputFileName := "userids.csv"
+	inputFile := openFile(inputFileName)
+
+	r := csv.NewReader(inputFile)
+	records, err := r.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
-	newFile.Close()
-}
 
-func OpenFile(inputFileName string) *os.File {
-	file, err := os.Open(inputFileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return file
-}
+	client := &http.Client{}
+	client.Timeout = time.Second * 15
 
-func WriteLine(inputFileName, line string) {
-	newFile, err := os.OpenFile(inputFileName, os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
+	var thisResp UserAPIResp
+	var respChan = make(chan UserAPIResp, 0)
+	var limiter = make(chan bool, 10)
+
+	for i := 0; i < len(records); i++ {
+		go func(reqUrl string, respChan chan UserAPIResp, limiter chan bool) {
+			limiter <- true
+			req, _ := http.NewRequest("GET", reqUrl, nil)
+			resp, _ := client.Do(req)
+			defer resp.Body.Close()
+			bodyArrBytes, readerr := ioutil.ReadAll(resp.Body)
+			if readerr != nil {
+				log.Printf("Error on decode bytes. %s", readerr)
+			}
+			unmarshalerr := json.Unmarshal(bodyArrBytes, &thisResp)
+			if unmarshalerr != nil {
+				log.Printf("Error on unmarshal. %s", unmarshalerr)
+			}
+			respChan <- thisResp
+		}(fmt.Sprintf("https://api.mercadolibre.com/users/%s",records[i][0]), respChan, limiter)
 	}
-	defer newFile.Close()
-	newFile.WriteString(line)
-	if err != nil {
-		panic(err)
+	for i := 0; i < len(records); i++ {
+		response := <-respChan
+		writeLine(fileName, fmt.Sprintf("%s", response.Nickname))
+		<- limiter
 	}
 }
